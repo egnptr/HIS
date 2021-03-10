ad789d937327af043f8bcc308c4af7040614afdb
Eugene Ekaputra <eugeneekaputra@gmail.com>
2021-03-10 22:03:45 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
+        <div class="w-3/4 bg-white p-9 rounded-lg">
             <div class="px-6 py-4 bg-white border-b border-gray-200 font-bold uppercase">
                 Dashboard
             </div>
