f94d2442ebe9dfcac2053f664465eb1f1c25f894
Eugene Ekaputra <eugeneekaputra@gmail.com>
2021-03-23 17:33:28 +0700

    Dashboard & UI changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,6 +24,30 @@
                     </div>
                 @endcannot
 
+                @cannot('staff')
+                    <div class="card bg-white shadow h-auto w-auto rounded-2xl overflow-hidden relative">
+                        <h1 class="shadow-md text-l font-bold text-center text-gray-600 uppercase p-3">Inpatient</h1>
+                        <img class="py-10 transform scale-150" src="{{ asset('img/patient.jpg') }}" alt="" />
+                        <button onclick="location.href='{{ route('patient') }}'" class="card bg-gray-700 hover:bg-gray-600 transition text-white w-full h-1/6 absolute bottom-0 ">Show table</button>
+                    </div>
+                @endcannot
+
+                @cannot('staff')
+                    <div class="card bg-white shadow h-auto w-auto rounded-2xl overflow-hidden relative">
+                        <h1 class="shadow-md text-l font-bold text-center text-gray-600 uppercase p-3">Outpatient</h1>
+                        <img class="py-10 transform scale-150" src="{{ asset('img/patient.jpg') }}" alt="" />
+                        <button onclick="location.href='{{ route('patient') }}'" class="card bg-gray-700 hover:bg-gray-600 transition text-white w-full h-1/6 absolute bottom-0 ">Show table</button>
+                    </div>
+                @endcannot
+
+                @canany(['admin', 'staff'])
+                <div class="card bg-white shadow h-auto w-auto rounded-2xl overflow-hidden relative">
+                    <h1 class="shadow-md text-l font-bold text-center text-gray-600 uppercase p-3">Pharmacy</h1>
+                    <img class="py-6 transform scale-125" src="{{ asset('img/pharmacy.jpeg') }}" alt="" />
+                    <button onclick="location.href='{{ route('receipt') }}'" class="card bg-gray-700 hover:bg-gray-600 transition text-white w-full h-1/6 absolute bottom-0 ">Show table</button>
+                </div>
+                @endcanany
+
                 @canany(['admin', 'staff'])
                 <div class="card bg-white shadow h-auto w-auto rounded-2xl overflow-hidden relative">
                     <h1 class="shadow-md text-l font-bold text-center text-gray-600 uppercase p-3">Patient Checkout</h1>
